7ebd9535d86918606e9db4397f464884bcda3dff
julian <julianx@gmail.com>
2023-06-15 10:29:39 -0400

    Adding new OOP logic for the words.

--- regular_cli_mode_enhanced.py
+++ regular_cli_mode_enhanced.py
@@ -6,318 +6,155 @@ from random import shuffle
 
 
 class Word(object):
-    kanji = ""
-    hiragana = ""
-    meaning = ""
-    challenge = ""
-    challenge_str = ""
-
-    def __init__(self, kwargs):
-        if "kanji" in kwargs.keys():
-            self.kanji = kwargs.get("kanji")
-            # self.challenge = self.kanji
-        if "hiragana" in kwargs.keys():
-            self.hiragana = kwargs.get("hiragana")
-        if "meaning" in kwargs.keys():
-            self.meaning = kwargs.get("meaning")
-
-        if self.hiragana and self.kanji:
-            self.challenge_str = f"{self.kanji} / {self.hiragana} "
-        elif self.hiragana:
-            self.challenge_str = f"{self.hiragana} "
-
-        if not self.challenge:
-            if self.hiragana:
-                self.challenge = self.hiragana
-            else:
-                self.challenge = self.kanji
+    def __init__(self, word, meaning=""):
+        self.word = word
+        self.meaning = meaning
+        self.challenge = self.word
+        self.challenge_str = f"{self.challenge} "
 
     def __str__(self):
-        return f"{self.kanji} {self.hiragana} {self.meaning}"
+        if self.meaning:
+            return f"{self.challenge_str} {self.meaning}"
+        else:
+            return f"{self.challenge_str}"
+
+    def check(self, answer):
+        return answer == self.challenge
+
+    def pronounce(self):
+        thread = threading.Thread(target=self.thread_function, args=(self.word,))
+        thread.start()
+
+    def thread_function(self):
+        if len(self.challenge) == 1:
+            subprocess.run(["say", self.challenge])
+        else:
+            command = ["say"] + self.challenge.split()
+            subprocess.run(command)
+
+
+class KanjiWord(Word):
+    def __init__(self, kanji, word, meaning):
+        super().__init__(word=word, meaning=meaning)
+        self.kanji = kanji
+
+        if self.kanji and self.word:
+            self.challenge = self.word
+            self.challenge_str = f"{self.kanji} / {self.word} "
 
 
 def populate_word_list():
     result = [
-        {"hiragana": "あ"},
-        {"hiragana": "い"},
-        {"hiragana": "う"},
-        {"hiragana": "え"},
-        {"hiragana": "お"},
-        {"hiragana": "か"},
-        {"hiragana": "き"},
-        {"hiragana": "く"},
-        {"hiragana": "け"},
-        {"hiragana": "こ"},
-        {"hiragana": "さ"},
-        {"hiragana": "し"},
-        {"hiragana": "す"},
-        {"hiragana": "せ"},
-        {"hiragana": "そ"},
-        {"hiragana": "た"},
-        {"hiragana": "ち"},
-        {"hiragana": "つ"},
-        {"hiragana": "て"},
-        {"hiragana": "と"},
-        {"hiragana": "な"},
-        {"hiragana": "に"},
-        {"hiragana": "ぬ"},
-        {"hiragana": "ね"},
-        {"hiragana": "の"},
-        {"hiragana": "は"},
-        {"hiragana": "ひ"},
-        {"hiragana": "ふ"},
-        {"hiragana": "へ"},
-        {"hiragana": "ほ"},
-        {"hiragana": "ま"},
-        {"hiragana": "み"},
-        {"hiragana": "む"},
-        {"hiragana": "め"},
-        {"hiragana": "も"},
-        {"hiragana": "ら"},
-        {"hiragana": "り"},
-        {"hiragana": "る"},
-        {"hiragana": "れ"},
-        {"hiragana": "ろ"},
-        {"hiragana": "わ"},
-        {"hiragana": "を"},
-        {"hiragana": "や"},
-        {"hiragana": "ゆ"},
-        {"hiragana": "よ"},
-        {"hiragana": "ん"},
+        Word(word="あ"), Word(word="い"), Word(word="う"), Word(word="え"), Word(word="お"),
+        Word(word="か"), Word(word="き"), Word(word="く"), Word(word="け"), Word(word="こ"),
+        Word(word="さ"), Word(word="し"), Word(word="す"), Word(word="せ"), Word(word="そ"),
+        Word(word="た"), Word(word="ち"), Word(word="つ"), Word(word="て"), Word(word="と"),
+        Word(word="な"), Word(word="に"), Word(word="ぬ"), Word(word="ね"), Word(word="の"),
+        Word(word="は"), Word(word="ひ"), Word(word="ふ"), Word(word="へ"), Word(word="ほ"),
+        Word(word="ま"), Word(word="み"), Word(word="む"), Word(word="め"), Word(word="も"),
+        Word(word="ら"), Word(word="り"), Word(word="る"), Word(word="れ"), Word(word="ろ"),
+        Word(word="わ"), Word(word="を"), Word(word="や"), Word(word="ゆ"), Word(word="よ"),
+        Word(word="ん"),
 
-        {"hiragana": "が"},
-        {"hiragana": "ぎ"},
-        {"hiragana": "ぐ"},
-        {"hiragana": "げ"},
-        {"hiragana": "ご"},
-        {"hiragana": "ざ"},
-        {"hiragana": "じ"},
-        {"hiragana": "ず"},
-        {"hiragana": "ぜ"},
-        {"hiragana": "ぞ"},
-        {"hiragana": "だ"},
-        {"hiragana": "ぢ"},
-        {"hiragana": "づ"},
-        {"hiragana": "で"},
-        {"hiragana": "ど"},
-        {"hiragana": "ば"},
-        {"hiragana": "び"},
-        {"hiragana": "ぶ"},
-        {"hiragana": "べ"},
-        {"hiragana": "ぼ"},
-        {"hiragana": "ぱ"},
-        {"hiragana": "ぴ"},
-        {"hiragana": "ぷ"},
-        {"hiragana": "ぺ"},
-        {"hiragana": "ぽ"},
+        Word(word="が"), Word(word="ぎ"), Word(word="ぐ"), Word(word="げ"), Word(word="ご"),
+        Word(word="ざ"), Word(word="じ"), Word(word="ず"), Word(word="ぜ"), Word(word="ぞ"),
+        Word(word="だ"), Word(word="ぢ"), Word(word="づ"), Word(word="で"), Word(word="ど"),
+        Word(word="ば"), Word(word="び"), Word(word="ぶ"), Word(word="べ"), Word(word="ぼ"),
+        Word(word="ぱ"), Word(word="ぴ"), Word(word="ぷ"), Word(word="ぺ"), Word(word="ぽ"),
 
-        {"hiragana": "きゃ"},
-        {"hiragana": "きゅ"},
-        {"hiragana": "きょ"},
-        {"hiragana": "しゃ"},
-        {"hiragana": "しゅ"},
-        {"hiragana": "しょ"},
-        {"hiragana": "ちゃ"},
-        {"hiragana": "ちゅ"},
-        {"hiragana": "ちょ"},
-        {"hiragana": "にゃ"},
-        {"hiragana": "にゅ"},
-        {"hiragana": "にょ"},
-        {"hiragana": "ひゃ"},
-        {"hiragana": "ひゅ"},
-        {"hiragana": "ひょ"},
-        {"hiragana": "みゃ"},
-        {"hiragana": "みゅ"},
-        {"hiragana": "みょ"},
-        {"hiragana": "りゃ"},
-        {"hiragana": "りゅ"},
-        {"hiragana": "りょ"},
-        {"hiragana": "ぎゃ"},
-        {"hiragana": "ぎゅ"},
-        {"hiragana": "ぎょ"},
-        {"hiragana": "じゃ"},
-        {"hiragana": "じゅ"},
-        {"hiragana": "じょ"},
-        {"hiragana": "びゃ"},
-        {"hiragana": "びゅ"},
-        {"hiragana": "びょ"},
-        {"hiragana": "ぴゃ"},
-        {"hiragana": "ぴゅ"},
-        {"hiragana": "ぴょ"},
+        Word(word="きゃ"), Word(word="きゅ"), Word(word="きょ"),
+        Word(word="しゃ"), Word(word="しゅ"), Word(word="しょ"),
+        Word(word="ちゃ"), Word(word="ちゅ"), Word(word="ちょ"),
+        Word(word="にゃ"), Word(word="にゅ"), Word(word="にょ"),
+        Word(word="ひゃ"), Word(word="ひゅ"), Word(word="ひょ"),
+        Word(word="みゃ"), Word(word="みゅ"), Word(word="みょ"),
+        Word(word="りゃ"), Word(word="りゅ"), Word(word="りょ"),
+        Word(word="ぎゃ"), Word(word="ぎゅ"), Word(word="ぎょ"),
+        Word(word="じゃ"), Word(word="じゅ"), Word(word="じょ"),
+        Word(word="びゃ"), Word(word="びゅ"), Word(word="びょ"),
+        Word(word="ぴゃ"), Word(word="ぴゅ"), Word(word="ぴょ"),
 
-        {"katakana": "ア"},
-        {"katakana": "イ"},
-        {"katakana": "ウ"},
-        {"katakana": "エ"},
-        {"katakana": "オ"},
-        {"katakana": "カ"},
-        {"katakana": "キ"},
-        {"katakana": "ク"},
-        {"katakana": "ケ"},
-        {"katakana": "コ"},
-        {"katakana": "サ"},
-        {"katakana": "シ"},
-        {"katakana": "ス"},
-        {"katakana": "セ"},
-        {"katakana": "ソ"},
-        {"katakana": "タ"},
-        {"katakana": "チ"},
-        {"katakana": "ツ"},
-        {"katakana": "テ"},
-        {"katakana": "ト"},
-        {"katakana": "ナ"},
-        {"katakana": "ニ"},
-        {"katakana": "ヌ"},
-        {"katakana": "ネ"},
-        {"katakana": "ノ"},
-        {"katakana": "ハ"},
-        {"katakana": "ヒ"},
-        {"katakana": "フ"},
-        {"katakana": "ヘ"},
-        {"katakana": "ホ"},
-        {"katakana": "マ"},
-        {"katakana": "ミ"},
-        {"katakana": "ム"},
-        {"katakana": "メ"},
-        {"katakana": "モ"},
-        {"katakana": "ヤ"},
-        {"katakana": "ユ"},
-        {"katakana": "ヨ"},
-        {"katakana": "ラ"},
-        {"katakana": "リ"},
-        {"katakana": "ル"},
-        {"katakana": "レ"},
-        {"katakana": "ロ"},
-        {"katakana": "ワ"},
-        {"katakana": "ヲ"},
-        {"katakana": "ン"},
-        {"katakana": "ガ"},
-        {"katakana": "ギ"},
-        {"katakana": "グ"},
-        {"katakana": "ゲ"},
-        {"katakana": "ゴ"},
-        {"katakana": "ザ"},
-        {"katakana": "ジ"},
-        {"katakana": "ズ"},
-        {"katakana": "ゼ"},
-        {"katakana": "ゾ"},
-        {"katakana": "ダ"},
-        {"katakana": "ヂ"},
-        {"katakana": "ヅ"},
-        {"katakana": "デ"},
-        {"katakana": "ド"},
-        {"katakana": "バ"},
-        {"katakana": "ビ"},
-        {"katakana": "ブ"},
-        {"katakana": "ベ"},
-        {"katakana": "ボ"},
-        {"katakana": "パ"},
-        {"katakana": "ピ"},
-        {"katakana": "プ"},
-        {"katakana": "ペ"},
-        {"katakana": "ポ"},
-        {"katakana": "キャ"},
-        {"katakana": "キュ"},
-        {"katakana": "キョ"},
-        {"katakana": "シャ"},
-        {"katakana": "シュ"},
-        {"katakana": "ショ"},
-        {"katakana": "チャ"},
-        {"katakana": "チュ"},
-        {"katakana": "チョ"},
-        {"katakana": "ニャ"},
-        {"katakana": "ニュ"},
-        {"katakana": "ニョ"},
-        {"katakana": "ヒャ"},
-        {"katakana": "ヒュ"},
-        {"katakana": "ヒョ"},
-        {"katakana": "ミャ"},
-        {"katakana": "ミュ"},
-        {"katakana": "ミョ"},
-        {"katakana": "リャ"},
-        {"katakana": "リュ"},
-        {"katakana": "リョ"},
-        {"katakana": "ギャ"},
-        {"katakana": "ギュ"},
-        {"katakana": "ギョ"},
-        {"katakana": "ジャ"},
-        {"katakana": "ジュ"},
-        {"katakana": "ジョ"},
-        {"katakana": "ビャ"},
-        {"katakana": "ビュ"},
-        {"katakana": "ビョ"},
-        {"katakana": "ピャ"},
-        {"katakana": "ピュ"},
-        {"katakana": "ピョ"},
+        Word(word="ア"), Word(word="イ"), Word(word="ウ"), Word(word="エ"), Word(word="オ"),
+        Word(word="カ"), Word(word="キ"), Word(word="ク"), Word(word="ケ"), Word(word="コ"),
+        Word(word="サ"), Word(word="シ"), Word(word="ス"), Word(word="セ"), Word(word="ソ"),
+        Word(word="タ"), Word(word="チ"), Word(word="ツ"), Word(word="テ"), Word(word="ト"),
+        Word(word="ナ"), Word(word="ニ"), Word(word="ヌ"), Word(word="ネ"), Word(word="ノ"),
+        Word(word="ハ"), Word(word="ヒ"), Word(word="フ"), Word(word="ヘ"), Word(word="ホ"),
+        Word(word="マ"), Word(word="ミ"), Word(word="ム"), Word(word="メ"), Word(word="モ"),
+        Word(word="ヤ"), Word(word="ユ"), Word(word="ヨ"), Word(word="ラ"), Word(word="リ"),
+        Word(word="ル"), Word(word="レ"), Word(word="ロ"), Word(word="ワ"), Word(word="ヲ"),
+        Word(word="ン"), Word(word="ガ"), Word(word="ギ"), Word(word="グ"), Word(word="ゲ"),
+        Word(word="ゴ"), Word(word="ザ"), Word(word="ジ"), Word(word="ズ"), Word(word="ゼ"),
+        Word(word="ゾ"), Word(word="ダ"), Word(word="ヂ"), Word(word="ヅ"), Word(word="デ"),
+        Word(word="ド"), Word(word="バ"), Word(word="ビ"), Word(word="ブ"), Word(word="ベ"),
+        Word(word="ボ"), Word(word="パ"), Word(word="ピ"), Word(word="プ"), Word(word="ペ"),
+        Word(word="ポ"),
+        Word(word="キャ"), Word(word="キュ"), Word(word="キョ"),
+        Word(word="シャ"), Word(word="シュ"), Word(word="ショ"),
+        Word(word="チャ"), Word(word="チュ"), Word(word="チョ"),
+        Word(word="ニャ"), Word(word="ニュ"), Word(word="ニョ"),
+        Word(word="ヒャ"), Word(word="ヒュ"), Word(word="ヒョ"),
+        Word(word="ミャ"), Word(word="ミュ"), Word(word="ミョ"),
+        Word(word="リャ"), Word(word="リュ"), Word(word="リョ"),
+        Word(word="ギャ"), Word(word="ギュ"), Word(word="ギョ"),
+        Word(word="ジャ"), Word(word="ジュ"), Word(word="ジョ"),
+        Word(word="ビャ"), Word(word="ビュ"), Word(word="ビョ"),
+        Word(word="ピャ"), Word(word="ピュ"), Word(word="ピョ"),
 
-    {"hiragana": "これ", "meaning": "This thing"},
-        {"hiragana": "この", "meaning": "This person"},
-        {"hiragana": "それ", "meaning": "That/it thing"},
-        {"hiragana": "その", "meaning": "That person"},
-        {"hiragana": "あれ", "meaning": "That over there"},
-        {"hiragana": "あの", "meaning": "That thing/person over there"},
-        {"kanji": "私", "hiragana": "わたし", "meaning": "I"},
-        {"hiragana": "あなた", "meaning": "You"},
-        {"kanji": "僕", "hiragana": "ぼく", "meaning": "me"},
-        {"kanji": "彼", "hiragana": "かれ", "meaning": "He"},
-        {"kanji": "彼女", "hiragana": "かのじょ", "meaning": "She"},
-        {"kanji": "私たち", "hiragana": "わたしたち", "meaning": "We"},
-        {"kanji": "彼ら", "hiragana": "かれら", "meaning": "They"},
+        Word(word="これ", meaning="This thing"),
+        Word(word="この", meaning="This person"),
+        Word(word="それ", meaning="That/it thing"),
+        Word(word="その", meaning="That person"),
+        Word(word="あれ", meaning="That over there"),
+        Word(word="あの", meaning="That thing/person over there"),
+        Word(word="あなた", meaning="You"),
+        KanjiWord(word="わたし", kanji="私", meaning="I"),
+        KanjiWord(word="ぼく", kanji="僕", meaning="me"),
+        KanjiWord(word="かれ", kanji="彼", meaning="He"),
+        KanjiWord(word="かのじょ", kanji="彼女", meaning="She"),
+        KanjiWord(word="わたしたち", kanji="私たち", meaning="We"),
+        KanjiWord(word="かれら", kanji="彼ら", meaning="They"),
 
-        {"kanji": "今日", "hiragana": "きょう", "meaning": "Today"},
-        {"kanji": "明日", "hiragana": "あした", "meaning": "Tomorrow"},
-        {"kanji": "昨日", "hiragana": "きのう", "meaning": "Yesterday"},
-        {"kanji": "今", "hiragana": "いま", "meaning": "Now"},
-        {"kanji": "前に", "hiragana": "まえに", "meaning": "Before"},
-        {"kanji": "後で", "hiragana": "あとで", "meaning": "Later"},
+        KanjiWord(word="きょう", kanji="今日", meaning="Today"),
+        KanjiWord(word="あした", kanji="明日", meaning="Tomorrow"),
+        KanjiWord(word="きのう", kanji="昨日", meaning="Yesterday"),
+        KanjiWord(word="いま", kanji="今", meaning="Now"),
+        KanjiWord(word="まえに", kanji="前に", meaning="Before"),
+        KanjiWord(word="あとで", kanji="後で", meaning="Later"),
 
-        {"hiragana": "する", "meaning": "To do"},
-        {"hiragana": "です", "meaning": "To be"},
-        {"hiragana": "なる", "meaning": "To become"},
-        {"hiragana": "ある", "meaning": "There is (inanimate)"},
-        {"hiragana": "いる", "meaning": "There is (living)"},
-        {"kanji": "行く", "hiragana": "いく", "meaning": "To go"},
-        {"kanji": "言う", "hiragana": "いう", "meaning": "To say"},
-        {"kanji": "見る", "hiragana": "みる", "meaning": "To see"},
-        {"kanji": "来る", "hiragana": "くる", "meaning": "To come"},
-        {"kanji": "食べる", "hiragana": "たべる", "meaning": "To eat"},
-        {"kanji": "遊ぶ", "hiragana": "あそぶ", "meaning": "to play"},
-        {"kanji": "寝る", "hiragana": "ねる", "meaning": "to sleep"},
-        {"kanji": "走る", "hiragana": "はしる", "meaning": "to run"},
-        {"kanji": "飛ぶ", "hiragana": "とぶ", "meaning": "to jump"},
-        {"kanji": "泣く", "hiragana": "なく", "meaning": "to cry"},
-        {"kanji": "笑う", "hiragana": "わらう", "meaning": "to laugh"},
-        {"kanji": "読む", "hiragana": "よむ", "meaning": "to read"},
-        {"kanji": "書く", "hiragana": "かく", "meaning": "to write"},
-        {"kanji": "描く", "hiragana": "かく", "meaning": "to draw"},
-
-        {"kanji": "飲む", "hiragana": "のむ", "meaning": "to drink"},
-        {"kanji": "起きる", "hiragana": "おきる", "meaning": "to wake up"},
-        {"kanji": "歩く", "hiragana": "あるく", "meaning": "to walk"},
-        {"kanji": "聞く", "hiragana": "きく", "meaning": "to listen"},
-        {"kanji": "待つ", "hiragana": "まつ", "meaning": "to wait"},
-        {"kanji": "泳ぐ", "hiragana": "およぐ", "meaning": "to swim"},
-        {"kanji": "歌う", "hiragana": "うたう", "meaning": "to sing"},
-        {"kanji": "音楽を聞く", "hiragana": "おんがくをきく", "meaning": "to listen to music"},
-        {"kanji": "絵を描く", "hiragana": "えをかく", "meaning": "to draw a picture"},
-        {"kanji": "洗う", "hiragana": "あらう", "meaning": "to wash"}
-
-
+        Word(word="する", meaning="To do"),
+        Word(word="です", meaning="To be"),
+        Word(word="なる", meaning="To become"),
+        Word(word="ある", meaning="There is (inanimate)"),
+        Word(word="いる", meaning="There is (living)"),
+        KanjiWord(word="いく", kanji="行く", meaning="To go"),
+        KanjiWord(word="いう", kanji="言う", meaning="To say"),
+        KanjiWord(word="みる", kanji="見る", meaning="To see"),
+        KanjiWord(word="くる", kanji="来る", meaning="To come"),
+        KanjiWord(word="たべる", kanji="食べる", meaning="To eat"),
+        KanjiWord(word="あそぶ", kanji="遊ぶ", meaning="to play"),
+        KanjiWord(word="ねる", kanji="寝る", meaning="to sleep"),
+        KanjiWord(word="はしる", kanji="走る", meaning="to run"),
+        KanjiWord(word="とぶ", kanji="飛ぶ", meaning="to jump"),
+        KanjiWord(word="なく", kanji="泣く", meaning="to cry"),
+        KanjiWord(word="わらう", kanji="笑う", meaning="to laugh"),
+        KanjiWord(word="よむ", kanji="読む", meaning="to read"),
+        KanjiWord(word="かく", kanji="書く", meaning="to write"),
+        KanjiWord(word="かく", kanji="描く", meaning="to draw"),
+        KanjiWord(word="のむ", kanji="飲む", meaning="to drink"),
+        KanjiWord(word="おきる", kanji="起きる", meaning="to wake up"),
+        KanjiWord(word="あるく", kanji="歩く", meaning="to walk"),
+        KanjiWord(word="きく", kanji="聞く", meaning="to listen"),
+        KanjiWord(word="まつ", kanji="待つ", meaning="to wait"),
+        KanjiWord(word="およぐ", kanji="泳ぐ", meaning="to swim"),
+        KanjiWord(word="うたう", kanji="歌う", meaning="to sing"),
+        KanjiWord(word="おんがくをきく", kanji="音楽を聞く", meaning="to listen to music"),
+        KanjiWord(word="えをかく", kanji="絵を描く", meaning="to draw a picture"),
+        KanjiWord(word="あらう", kanji="洗う", meaning="to wash")
     ]
 
     random.shuffle(result)
     return result
-
-
-def thread_function(value):
-    if value.hiragana:
-        value = value.hiragana
-
-    if len(value) == 1:
-        subprocess.run(["say", value])
-    else:
-        command = ["say"] + value.split()
-        subprocess.run(command)
 
 
 def main(argv):
