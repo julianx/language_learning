e08fbe110f9f9d9451d1a2cf3ad9f1d49f7e77d1
julian <julianx@gmail.com>
2023-06-15 08:52:09 -0400

    Adding base verbs.

--- regular_cli_mode_enhanced.py
+++ regular_cli_mode_enhanced.py
@@ -10,21 +10,27 @@ class Word(object):
     hiragana = ""
     meaning = ""
     challenge = ""
+    challenge_str = ""
 
     def __init__(self, kwargs):
         if "kanji" in kwargs.keys():
             self.kanji = kwargs.get("kanji")
-            self.challenge = self.kanji
+            # self.challenge = self.kanji
         if "hiragana" in kwargs.keys():
             self.hiragana = kwargs.get("hiragana")
         if "meaning" in kwargs.keys():
             self.meaning = kwargs.get("meaning")
 
+        if self.hiragana and self.kanji:
+            self.challenge_str = f"{self.kanji} / {self.hiragana} "
+        elif self.hiragana:
+            self.challenge_str = f"{self.hiragana} "
+
         if not self.challenge:
             if self.hiragana:
                 self.challenge = self.hiragana
             else:
-                self.challenge = "error"
+                self.challenge = self.kanji
 
     def __str__(self):
         return f"{self.kanji} {self.hiragana} {self.meaning}"
@@ -84,12 +90,31 @@ def populate_word_list():
         {"hiragana": "その", "meaning": "That person"},
         {"hiragana": "あれ", "meaning": "That over there"},
         {"hiragana": "あの", "meaning": "That thing/person over there"},
-        {"kanji": "私", "hiragana": "わたし", "meaning": "me"},
+        {"kanji": "私", "hiragana": "わたし", "meaning": "I"},
+        {"hiragana": "あなた", "meaning": "You"},
         {"kanji": "僕", "hiragana": "ぼく", "meaning": "me"},
         {"kanji": "彼", "hiragana": "かれ", "meaning": "He"},
         {"kanji": "彼女", "hiragana": "かのじょ", "meaning": "She"},
         {"kanji": "私たち", "hiragana": "わたしたち", "meaning": "We"},
         {"kanji": "彼ら", "hiragana": "かれら", "meaning": "They"},
+
+        {"kanji": "今日", "hiragana": "きょう", "meaning": "Today"},
+        {"kanji": "明日", "hiragana": "あした", "meaning": "Tomorrow"},
+        {"kanji": "昨日", "hiragana": "きのう", "meaning": "Yesterday"},
+        {"kanji": "今", "hiragana": "いま", "meaning": "Now"},
+        {"kanji": "前に", "hiragana": "まえに", "meaning": "Before"},
+        {"kanji": "後で", "hiragana": "あとで", "meaning": "Later"},
+
+        {"hiragana": "する", "meaning": "To do"},
+        {"hiragana": "です", "meaning": "To be"},
+        {"hiragana": "なる", "meaning": "To become"},
+        {"hiragana": "ある", "meaning": "There is (inanimate)"},
+        {"hiragana": "いる", "meaning": "There is (living)"},
+        {"kanji": "行く", "hiragana": "いく", "meaning": "To go"},
+        {"kanji": "言う", "hiragana": "いう", "meaning": "To say"},
+        {"kanji": "見る", "hiragana": "みる", "meaning": "To see"},
+        {"kanji": "来る", "hiragana": "くる", "meaning": "To come"},
+        {"kanji": "食べる", "hiragana": "たべる", "meaning": "To eat"},
     ]
 
     random.shuffle(result)
@@ -97,9 +122,7 @@ def populate_word_list():
 
 
 def thread_function(value):
-    if value.kanji:
-        value = value.kanji
-    elif value.hiragana:
+    if value.hiragana:
         value = value.hiragana
 
     if len(value) == 1:
@@ -124,12 +147,12 @@ def main(argv):
         for word in game_list:
             print(f"Score: {score}")
             thread = threading.Thread(target=thread_function, args=(word,))
-            answer = input(word.challenge)
+            answer = input(word.challenge_str)
             if answer in ["q", "Q", "quit"]:
                 done = True
             elif answer == word.hiragana:
                 score += 1
-                print("Correct")
+                print("Correct", word)
             else:
                 score -= 10
                 print("Wrong!", word)
